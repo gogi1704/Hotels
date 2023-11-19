e0cdaa7b3fb5182bbce7980ebecbbe7a9c3441e9
George <96864675+gogi1704@users.noreply.github.com>
2023-11-20 02:00:04 +0300

    update CompleteFragment and small fixes

--- app/src/main/java/com/l_george/hotels/ui/fragments/CompleteFragment.kt
+++ app/src/main/java/com/l_george/hotels/ui/fragments/CompleteFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.l_george.hotels.R
 import com.l_george.hotels.databinding.FragmentCompleteBinding
+import kotlin.random.Random
 
 class CompleteFragment : Fragment() {
     private lateinit var binding: FragmentCompleteBinding
@@ -13,7 +17,33 @@ class CompleteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCompleteBinding.inflate(layoutInflater , container , false)
+        binding = FragmentCompleteBinding.inflate(layoutInflater, container, false)
+
+        with(binding) {
+            textReservNum.text = StringBuilder()
+                .append( requireActivity().getString(R.string.text_reserv_part1))
+                .append(Random.nextInt(100_000, 1_000_000))
+                .append(" ")
+                .append(requireActivity().getString(R.string.text_reserv_part2))
+
+            buttonBack.setOnClickListener {
+                findNavController().navigateUp()
+            }
+
+            buttonComplete.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_completeFragment_to_hotelsFragment,null,
+                    NavOptions.Builder()
+                        .setEnterAnim(R.anim.in_right)
+                        .setExitAnim(R.anim.out_left)
+                        .setPopEnterAnim(R.anim.in_left)
+                        .setPopExitAnim(R.anim.out_right)
+                        .setPopUpTo(R.id.hotelsFragment, true).build()
+                )
+
+            }
+        }
+
         return binding.root
     }
 
